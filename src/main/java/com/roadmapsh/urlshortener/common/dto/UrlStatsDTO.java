f2ad55e6ba9ab90ce28a708f314b1cf6cc7b5717
rcc-castillo <rcc1591@gmail.com>
2024-11-07 19:45:38 +0100

    Added model, repository and service layers Added

--- src/main/java/com/roadmapsh/urlshortener/common/dto/UrlStatsDTO.java
+++ src/main/java/com/roadmapsh/urlshortener/common/dto/UrlStatsDTO.java
@@ -1,14 +1,7 @@
 package com.roadmapsh.urlshortener.common.dto;
 
-
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
